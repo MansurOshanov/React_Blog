6b7c3e741c70526f9042324c6151a4339d50f567
Mans <mansur97.97@gmail.com>
2021-07-14 15:30:54 +0600

    Inital components idea

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import * as serviceWorker from './serviceWorker';
+import './index.css'
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
+import App from './App'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const routing = (
+  <Router>
+    <React.StrictMode>
+      <Header></Header>
+      <Switch>
+        <Route exact path='/' component={App} ></Route>
+        <Footer></Footer>
+      </Switch>
+    </React.StrictMode>
+  </Router>
+)
