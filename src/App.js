2b2ac6915651e6ba7b592d8cdc15856a1e3a5c6e
Mans <mansur97.97@gmail.com>
2021-12-29 00:13:00 +0600

    created axiosInstance

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-
 import './App.css';
+import Posts from './components/Posts';
+import { useEffect, useState } from 'react';
+import PostLoadingComponent from './components/PostLoading';
 
 function App() {
+  const PostLoading = PostLoadingComponent(Posts);
+  const [appState, setAppState] = useState({
+    isLoading: false,
+    posts: null
+  });
+  
+  useEffect(()=>{
+    setAppState({isLoading: true});
+    const apiUrl = 'http://127.0.0.1:8000/api/'
+    fetch(apiUrl)
+      .then((data) => data.json())
+      .then((posts) => {
+        setAppState({
+          isLoading: false,
+          posts: posts
+        });
+      });
+  }, [setAppState]);
+
   return (
     <div className="App">
+      <PostLoading isLoading={appState.isLoading} posts={appState.posts} ></PostLoading>
         
-        <h1>h1</h1>
-        Hello world
     </div>
   );
 }
