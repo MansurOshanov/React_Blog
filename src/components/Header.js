5481620a0783c54e36a278972f43acd651b0ae61
Mans <mansur97.97@gmail.com>
2021-12-29 01:13:03 +0600

    created register component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,20 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { makeStyles } from '@material-ui/core/styles'
+import { NavLink } from 'react-router-dom'
+import Link from '@material-ui/core/Link'
+import Button from'@material-ui/core/Button'
 
 const useStyles = makeStyles((theme) => ({
     appBar: {
-        borderBottom: '1px solid ${theme.palette.divider}'
-    },
+		borderBottom: `1px solid ${theme.palette.divider}`,
+	},
+	link: {
+		margin: theme.spacing(1, 1.5),
+	},
+	toolbarTitle: {
+		flexGrow: 1,
+	},
 }));
 
 function Header() {
@@ -27,13 +36,50 @@ function Header() {
                         variant = 'h6'
                         color = 'inherit'
                         noWrap>
-                        Mans' blog
+                            <Link 
+                                component={NavLink}
+                                to='/'
+                                underline='none'
+                                color='textPrimary'
+                            > Mans' blog
+                        </Link>
                     </Typography>
+                    <nav>
+                        <Link
+                            color='textPrimary'
+                            href='#'
+                            className={classes.link}
+                            component={NavLink}
+                            to='/register'
+                        >
+                            Register
+                        </Link>
+                    </nav>
+                    <Button
+						href="#"
+						color="primary"
+						variant="outlined"
+						className={classes.link}
+						component={NavLink}
+						to="/login"
+					>
+						Login
+					</Button>
+					<Button
+						href="#"
+						color="primary"
+						variant="outlined"
+						className={classes.link}
+						component={NavLink}
+						to="/logout"
+					>
+						Logout
+					</Button>
                 </Toolbar>
 
             </AppBar>
         </React.Fragment>
     )
-}
+};
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import * as serviceWorker from './serviceWorker';
-import './index.css'
-import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
-import App from './App'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import './index.css';
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+import App from './App';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Register from './components/register';
 
 const routing = (
   <Router>
@@ -13,10 +14,11 @@ const routing = (
       <Header></Header>
       <Switch>
         <Route exact path='/' component={App} ></Route>
+        <Route path='/register' component={Register}></Route>
       </Switch>
       <Footer></Footer>
     </React.StrictMode>
   </Router>
-)
+);
 
-ReactDOM.render(routing, document.getElementById('root'))
+ReactDOM.render(routing, document.getElementById('root'));
