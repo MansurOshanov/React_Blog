5db7d003a20ca58c1c63706578071be67b16209b
Mans <mansur97.97@gmail.com>
2021-12-29 01:23:00 +0600

    created logout component

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Register from './components/register';
 import Login from './components/login';
+import Logout from './components/login';
 
 const routing = (
   <Router>
@@ -17,6 +18,7 @@ const routing = (
         <Route exact path='/' component={App} ></Route>
         <Route path='/register' component={Register}></Route>
         <Route path='/login' component={Login}></Route>
+        <Route path='/logout' component={Logout}></Route>
       </Switch>
       <Footer></Footer>
     </React.StrictMode>
